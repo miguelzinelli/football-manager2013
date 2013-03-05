dafae05fcc5984e603c58ffdda8f186dee4b6bc7
Miguel <Miguel@10.2.9.45>
2013-03-05 10:00:02 -0300

    fs

--- src/br/edu/unipampa/clube/Clube.java
+++ src/br/edu/unipampa/clube/Clube.java
@@ -111,10 +111,7 @@ public class Clube implements Serializable {
     public void addJogadorDefesa(Jogador jogador){
         this.defesa.add(jogador);
         this.escalacaoReserva.add(jogador);
-        descontaValorJogador(jogador);
-       
-    }
-    public void descontaValorJogador(Jogador jogador){
         this.financas -= jogador.getValor();
+       
     }
 }
